e418d69ee6df937b73b7ac8cc523829f1d0b15d9
shegoo <kessi.k@edu.spbstu.ru>
2026-09-13 14:17:04 +0300

    fixed solution.js

--- kessi.kenza/lab2/solution.js
+++ kessi.kenza/lab2/solution.js
@@ -1,4 +1,9 @@
 export function countVowels(str) {
+  // Check if the input is not a string
+  if (typeof str !== 'string') {
+    return 0;
+  }
+
   const vowels = ['a', 'e', 'i', 'o', 'u'];
 
   return str
